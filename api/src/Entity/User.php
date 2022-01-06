d3c6f7e01421a90a4f4b0ab2ca641217507e5a08
Mikhail Ilinsky <mail@milinsky.ru>
2022-01-06 14:57:21 +1000

    Add User entity

---
+++ api/src/Entity/User.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`users`')]
+#[UniqueEntity("phone")]
+#[ApiResource(iri: 'http://schema.org/User')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 5, max: 100)]
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    #[Assert\Length(max: 300)]
+    #[ORM\Column(type: 'string')]
+    private string $avatar;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 10, max: 10)]
+    #[ORM\Column(type: 'integer')]
+    private int $phone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAvatar(): string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(string $avatar): self
+    {
+        $this->avatar = $avatar;
+        return $this;
+    }
+
+    public function getPhone(): int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+}
